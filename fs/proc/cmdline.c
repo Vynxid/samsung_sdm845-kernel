30cecef8f937cd40a3ad3bc0af69fff49462284a
Andrey0800770 <andrey0800770@gmail.com>
2024-12-16 18:24:59 -0300

    add susfs

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -3,11 +3,23 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
+#ifdef CONFIG_KSU_SUSFS_SPOOF_PROC_CMDLINE
+extern int susfs_spoof_proc_cmdline(struct seq_file *m);
+#endif
+
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
+#ifdef CONFIG_KSU_SUSFS_SPOOF_PROC_CMDLINE
+        if (!susfs_spoof_proc_cmdline(m)) {
+                seq_putc(m, '\n');
+                return 0;
+        }
+#endif
 	seq_printf(m, "%s\n", saved_command_line);
+
 	return 0;
 }
+
 
 static int cmdline_proc_open(struct inode *inode, struct file *file)
 {
